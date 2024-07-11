a3fee01b4aefce43da14d9bf41c7998b2217d04c
son12039 <sonson12039@gmail.com>
2024-07-11 22:32:53 +0900

    07_11home

--- 2_Java/11_Exception/src/com/kh/practice999/Application.java
+++ 2_Java/11_Exception/src/com/kh/practice999/Application.java
@@ -12,109 +12,121 @@ import com.kh.practice999.model.Music;
 public class Application {
 	static Controller con = new Controller();
 	static Scanner sc = new Scanner(System.in);
-	static Application app = new Application();
-	static ArrayList<ArrayList<String>> musiclist= new ArrayList();
+	
 	public static void main(String[] args) {
-		System.out.println(musiclist);
+		Application app = new Application();
 	boolean check = true;
 	while(check) {
-		int menunum = app.mainmenu();
-		switch (menunum) {
-	case 1 : app.lastadd();break;
-	case 2 : app.firstadd();break;
-	case 3 : app.Allprint();break;
-	case 4 : app.search();break;
-	case 5 : app.delete();break;
-	case 6 : app.rewrite();break;
-	case 7 : app.singerset();break;
-	case 9 : System.out.println("종료");check = false;
+		
+		try {	
+			int menunum = app.mainmenu();
+			switch (menunum) {
+		case 1 : app.lastadd();break;
+		case 2 : app.firstadd();break;
+		case 3 : app.Allprint();break;
+		case 4 : app.search();break;
+		case 5 : app.delete();break;
+		case 6 : app.rewrite();break;
+		case 7 : app.singerdownset();break;
+		case 8 : app.singerupset();break;
+		case 9 : System.out.println("종료");check = false;
+		default : System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");				
 			}
+		} catch (NumberFormatException e) {
+			System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
+			}
+		
 		}
 	}
-	
-	
 	
 	public int mainmenu() {System.out.println("===== 메인 메뉴 =====\n"+ "1. 마지막 위치에 곡 추가\r\n"
 			+ "2. 첫 위치에 곡 추가\r\n"+ "3. 전체 곡 목록 출력\r\n"
 			+ "4. 특정 곡 검색\r\n"+ "5. 특정 곡 삭제\r\n"
 			+ "6. 특정 곡 수정\r\n"+ "7. 가수 명 내림차순 정렬\r\n"
-			+ "9. 종료");
+			+ "8. 가수 명 오름차순 정렬\r\n"+"9. 종료");
 			System.out.print("메뉴 번호 입력:");
 		
 			return Integer.parseInt(sc.nextLine());
 	}
 	public void lastadd() {
 		ArrayList<String> music= new ArrayList();
-		System.out.print("곡 명:");music.add(sc.nextLine());
-		System.out.print("가수 명:");music.add(sc.nextLine());
-		musiclist.addLast(music);
+		System.out.print("곡 명:");
+		String Music = sc.nextLine();
+		System.out.print("가수 명:");
+		String Singer = sc.nextLine();
+		con.lastadd(Music, Singer);
+		System.out.println("추가 성공");
 	}	
 	
 	public void firstadd() {
 		ArrayList<String> music= new ArrayList();
-		System.out.print("곡 명:");music.add(sc.nextLine());
-		System.out.print("가수 명:");music.add(sc.nextLine());
-		musiclist.addFirst(music);
+		System.out.print("곡 명:");
+		String Music = sc.nextLine();
+		System.out.print("가수 명:");
+		String Singer = sc.nextLine();
+		con.firstadd(Music, Singer);
+		System.out.println("추가 성공");
 	}
 	public void Allprint() {
-		System.out.println("***** 전체 곡 목록 출력 *****");
-		System.out.println(con.print());
-		for(int i = 0; i<musiclist.size(); i++) {
-			System.out.println(musiclist.get(i).get(1)+ "-"+musiclist.get(i).get(0));
+		System.out.println("***** 전체 곡 목록 출력 *****");		
+		for(Music Allprint : con.Allprint()) {
+			System.out.println(Allprint.getSinger() + "-" + Allprint.getTitle());
 		}
 	}
 	public void search() {
-		System.out.println("***** 특정 곡 검색 *****");
+		System.out.println("***** 특정 곡 검색(일부만 검색해도 가능) *****");
 		String search = sc.nextLine();
-		for(int i = 0; i<musiclist.size()+1; i++) {
-			if(i ==musiclist.size()) {System.out.println("없는 노래입니다");break;}
-			if(musiclist.get(i).get(0).equals(search)) {
-				System.out.println(musiclist.get(i).get(1)+ "-" +
-			musiclist.get(i).get(0)+"을 검색했습니다.");break;
-			}
+		
+		try {
+			System.out.println(con.search(search).getSinger()+ "-" +
+					con.search(search).getTitle()+ " 을/를 검색했습니다");
+		} catch (NullPointerException e) {
+			System.out.println("없는 노래입니다");
 		}
-
 	}
+
 	public void delete() {
 		System.out.println("***** 특정 곡 삭제 *****");
 		System.out.print("삭제할 곡 명:");
-		String delete = sc.nextLine();
-		for(int i = 0; i<musiclist.size()+1; i++) {
-			if(i ==musiclist.size()) {System.out.println("삭제할 곡이 없습니다");break;}
-			if(musiclist.get(i).get(0).equals(delete)) {
-				System.out.println(musiclist.get(i).get(1)+ "-" +
-			musiclist.get(i).get(0)+"을 삭제했습니다.");
-				musiclist.remove(i);break;
-			}
-		}
-		
+		String delete = sc.nextLine();	
+		 Music deletemusic = con.delete(delete);
+		if (deletemusic != null) {
+	        System.out.println(deletemusic.getSinger() + "-" + deletemusic.getTitle() + " 을/를 삭제했습니다");
+	    } else {
+	        System.out.println("삭제할 곡이 없습니다.");
+	    }
 	}
+	
 	public void rewrite() {
-		ArrayList<String> music= new ArrayList();
-		System.out.print("검색할 곡 명:");
-		String rewrite = sc.nextLine();
-		for(int i = 0; i<musiclist.size()+1; i++) {
-			if(i ==musiclist.size()) {System.out.println("수정할 곡을 찾지 못했습니다");break;}
-			if(musiclist.get(i).get(0).equals(rewrite)) {
-				System.out.print("수정할 곡 명:");music.add(sc.nextLine());
-				System.out.print("수정할 가수 명:");music.add(sc.nextLine());
-				System.out.println(musiclist.get(i).get(1)+ "-" +
-						musiclist.get(i).get(0)+"의 값이 변경되었습니다.");
-				musiclist.remove(i);
-				musiclist.add(i,music);			
-				break;
-			}
+		System.out.print("검색할 곡 명(곡이름과 완전히 일치해주세요):");
+		String rewritetarget = sc.nextLine();
+		System.out.print("수정할 곡 명:");
+		String rewritetitle = sc.nextLine();
+		System.out.print("수정할 가수 명:");
+		String rewritesinger = sc.nextLine();
+		Music beforemusic = con.rewrite(rewritetarget,rewritetitle, rewritesinger);
+		if(beforemusic!=null)
+		{System.out.println(beforemusic.getSinger()+"-"+beforemusic.getTitle()+"의 값이 변경되었습니다");}
+		else System.out.println("수정할 곡을 찾지 못했습니다");
 		}
+	
+	public void singerdownset() {
+		 System.out.println("***** 가수 명 내림차순 정렬 *****");
+		 con.singerdownset();
+		 ArrayList<Music> setmusic = con.Allprint();
+		    for (Music set : setmusic) {
+		        System.out.println(set.getSinger() + " - " + 
+		        		set.getTitle());
+		 }
 	}
-	public void singerset() {
-
-
-		 System.out.println("***** 노래 제목 내림차순 정렬 *****");
-		    for (ArrayList<String> playlist : musiclist) {
-		        System.out.println(playlist.get(1) + " - " + playlist.get(0));
-		    }
+	
+	public void singerupset() {
+		 System.out.println("***** 가수 명 오름차순 정렬 *****");
+		 con.singerupset();
+		 ArrayList<Music> setmusic = con.Allprint();
+		    for (Music set : setmusic) {
+		        System.out.println(set.getSinger() + " - " + 
+		        		set.getTitle());
+		 }
 	}
-	
-	
-	
 }
--- 2_Java/11_Exception/src/com/kh/practice999/controller/Controller.java
+++ 2_Java/11_Exception/src/com/kh/practice999/controller/Controller.java
@@ -1,24 +1,83 @@
 package com.kh.practice999.controller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import com.kh.practice999.model.Music;
 
 public class Controller {
     ArrayList<Music> musiclist = new ArrayList<>();
     Music music = new Music();
-
+    
+    
+    
     public void lastadd(String title, String singer) {
-        Music newMusic = new Music(title, singer);
-        musiclist.add(newMusic);
+        Music addMusic = new Music(title, singer);
+        musiclist.add(musiclist.size(),addMusic);
+    }
+    
+    public void firstadd(String title, String singer) {
+    	Music addMusic = new Music(title, singer);
+        musiclist.add(0, addMusic);
     }
 
-    public String print() {
-        StringBuilder sb = new StringBuilder();
-        for (Music m : musiclist) {
-            sb.append(m.toString()).append("\n");
+    public ArrayList<Music> Allprint() {       
+        return musiclist;
+    }
+    
+    public Music search(String title) {
+    	for(Music search : musiclist) {
+    		if(search.getTitle().contains(title))return search;
+    	}
+		return null;
+    }
+    
+    public Music delete(String title) {
+    	 Music deleteMusic = null;
+    	for (Music music : musiclist) {
+            if (music.getTitle().equals(title)) {
+                deleteMusic = music;
+                break;
+            }
         }
-        return sb.toString();
+        if (deleteMusic != null) {
+            musiclist.remove(deleteMusic);
+        }
+		return deleteMusic;
+    }
+    
+    public Music rewrite(String rewritett,String title,String singer) {
+    	 Music beforeMusic = null;
+    	for(Music rewritetitle : musiclist) {
+    		if(rewritetitle.getTitle().equals(rewritett)) {
+    			beforeMusic = new Music(rewritetitle.getTitle(),rewritetitle.getSinger());
+    			rewritetitle.setSinger(singer);
+    			rewritetitle.setTitle(title);
+    			break;
+    		}
+
+    	}
+		return beforeMusic;    	
+    }
+    
+    public void singerdownset() {
+        musiclist.sort(new Comparator<Music>() {  	
+            @Override
+            public int compare(Music m1, Music m2) {
+                return m2.getSinger().compareTo(m1.getSinger());
+            	}
+        	}
+       );
+    }
+    
+    public void singerupset() {
+        musiclist.sort(new Comparator<Music>() {  	
+            @Override
+            public int compare(Music m1, Music m2) {
+                return m1.getSinger().compareTo(m2.getSinger());
+            	}
+        	}
+       );
     }
 }
 
